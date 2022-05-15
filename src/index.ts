ffc2f32238ae1b7268d5102444bfe0b7e60ef753
sinh343 <yosefsuga@hotmail.co.uk>
2022-05-15 13:03:53 +0100

    fix guild members timeout error

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const reactionBotConfig: ReactionBotConfig = {
 }
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_MEMBERS]
 });
 
 
